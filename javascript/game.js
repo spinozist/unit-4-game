cd272d7c1cc081d8999277e2144ea46011efcf77
Spinozist <ewoodworth@atlantaregional.org>
2018-09-07 15:19:11 -0400

    Game works

--- javascript/game.js
+++ javascript/game.js
@@ -1,8 +1,14 @@
-var crystalArray = [];
-
-
 $("document").ready(function () {
+    var crystalArray = [];
+    var sumGuesses = 0;
     var matchRand = Math.floor(Math.random() * 251) + 100;
+    var drawReset = function () {
+        resetButton = "<input id='resetButton' type='button' value='Play Again' onClick='window.location.reload()'>";
+        $("#reset-box").html(resetButton);
+        document.onkeydown = function (event) {
+            event.preventDefault();
+        };
+    }
 
     $("#match-number").text(matchRand);
 
@@ -11,12 +17,29 @@ $("document").ready(function () {
         crystalArray.push(crystalRand);
         var button = $("<button>");
         button.attr("id", "button" + i);
+        button.attr("value", crystalRand)
         $(button).css("background-image", "url(assets/images/crystal-" + i + ".png");
         $("#button-row").append(button);
     }
 
-    $("#guess-number").text(0);
+    $("#guess-number").text(sumGuesses);
 
     console.log("ready :)");
 
+    $("button").on("click", function () {
+        sumGuesses = +sumGuesses + +this.value;
+        $("#guess-number").text(sumGuesses);
+
+        if (sumGuesses === matchRand) {
+            $("#wins-losses").html("<h1>You won!</h1>");
+            drawReset();
+        }
+
+        if (sumGuesses > matchRand) {
+            $("#wins-losses").html("<h1>You lose!</h1>");
+            drawReset();
+        }
+    });
 });
+
+
